171cd5e3b7ec3aa8107b2b742956975ba7b43715
Plantin7 <vincent.agullo1@gmail.com>
2020-05-05 02:14:43 +0200

    Project configuration in sub-modules

---
+++ Soundroid/app/src/main/java/fr/uge/soundroid/services/MusicPlayerService.kt
@@ -0,0 +1,12 @@
+package fr.uge.soundroid.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MusicPlayerService : Service() {
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
