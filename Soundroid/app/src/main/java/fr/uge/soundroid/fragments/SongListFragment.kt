05c5f2ba9cab54aef4e0aab32a5a3eb2d47f13e6
Plantin7 <vincent.agullo1@gmail.com>
2020-05-10 04:17:16 +0200

    Change simple main activity to tabbed Activity

--- Soundroid/app/src/main/java/fr/uge/soundroid/fragments/SongListFragment.kt
+++ Soundroid/app/src/main/java/fr/uge/soundroid/fragments/SongListFragment.kt
@@ -5,11 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import fr.uge.soundroid.R
+import fr.uge.soundroid.adapters.SongListAdapter
+import fr.uge.soundroid.models.Song
+
 
 class SongListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_song_list, container, false);
+        val rootView = inflater.inflate(R.layout.fragment_song_list, container, false)
+
+        val rv = rootView.findViewById(R.id.recyclerView) as RecyclerView
+        rv.setHasFixedSize(true)
+        val adapter = SongListAdapter(listOf(Song("A", "B", "C", "D")))
+        rv.adapter = adapter
+
+        val llm = LinearLayoutManager(activity)
+        rv.layoutManager = llm
+
+        return rootView
     }
 }
